b620f232799f344ea95fd5db8b69cc1cadca89cb
premdd230 <premdd230@gmail.com>
2025-10-07 09:36:46 -0700

    operators and variables

---
+++ public/script/intro.js
@@ -0,0 +1,45 @@
+let toy = "(let) string is a collection of characters";
+toy = "toy over write";
+//let toy = "toy over write"; // let : local scope variable and var is a global scope variable
+console.log(toy);
+const count = 4;
+//count = 2;
+console.log("reasign the count " + count);
+var car = "volvo";
+var car = "audi";
+console.log("var reasign "+car);
+//let char = a; // a through the undefined error
+let char = 'a';
+console.log(char);
+let _name = 123;
+let $$ ="dollar "
+console.log($$);
+// _underscore simple and $ dollar simple are allowed in the variable name
+let home;
+//if you console log the home , this through the undefined error
+// opperators
+// = ,+-/,
+console.log(10+9);
+console.log(10-9);
+console.log(10*9);
+console.log(10/9);
+console.log(10%5); //modules
+
+// logical operators && ,!, ||
+
+x = 8; y = 5;
+console.log("logical opperator"+x + " " + y );
+console.log(x < 10 && y > 1);
+console.log(x === 5 || y === 5);
+console.log(!(x === y) );
+
+let boolean = true;
+console.log(!boolean);
+
+//comparision operator 
+
+
+
+
+
+
